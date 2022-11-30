290d98a47f014b6058c0729cd23e44055f6edca5
Andi Wicaksono Nugroho <andiwn121@gmail.com>
2022-11-30 19:16:42 +0700

    update

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -35,7 +35,6 @@
         
 @endswitch
 <div class="main-sidebar">
-<<<<<<< HEAD
     <aside id="sidebar-wrapper">
       <div class="sidebar-brand">
         <a href="/home">Monitoring</a>
@@ -49,9 +48,6 @@
           <li class="nav-link {{ $ujian }}"><a class="nav-link" href="/ujian"><i class="fas fa-folder-open"></i> <span>Data Ujian</span></a></li>
           <li class="nav-link {{ $siswa }}"><a class="nav-link" href="/siswa"><i class="fas fa-folder-open"></i> <span>Data Siswa</span></a></li>
           <li class="nav-link {{ $admin }}"><a class="nav-link" href="/admin"><i class="fas fa-user-cog"></i> <span>Data Admin</span></a></li>
-=======
-  
->>>>>>> e1f3095353ae8c6bd9b490f35916fc2664d3e076
 
 
       {{-- <li class="nav-item">
