fbb2c2bfca63567748c71962391481d653d3131b
Andi Wicaksono Nugroho <andiwn121@gmail.com>
2022-12-08 10:09:19 +0700

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,24 @@
     <div class="section-header">
         <h1>Dashboard</h1>
     </div>
+    <div class="row justify-content-center">
+       <div class="col-md-8">
+           <div class="card">
+               <div class="card-body">
+                   @if (session('status'))
+                       <div class="alert alert-success" role="alert">
+                           {{ session('status') }}
+                       </div>
+                   @endif
+    
+                   {{ __('You are logged in!') }}
+               </div>
+           </div>
+       </div>
+    </div>
     <div class="container">
         <div class="row">
-
+            
             <div class="col-4">
                 <div class="card">
                     <div class="card-body">
@@ -98,21 +113,6 @@
                 </div>
             </div>
         </div>
-        <!-- <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        {{ __('You are logged in!') }}
-                    </div>
-                </div>
-            </div>
-        </div> -->
     </div>
 </section>
 @endsection
